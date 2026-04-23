1ba175d9dc9d7985990eda5723e62c393a423c75
AnaCarolinedx <ana.caroline.3047@gmail.com>
2026-04-22 22:33:16 -0300

    aula 22/04

---
+++ src/model/Cliente.js
@@ -0,0 +1,10 @@
+class Clientes{
+    constructor(id, nome, telefone, endereço){
+        this.id = id;
+        this.nome = nome;
+        this.telefone = telefone;
+        this.endereço = endereço;
+    }
+}
+
+module.exports = Clientes;
